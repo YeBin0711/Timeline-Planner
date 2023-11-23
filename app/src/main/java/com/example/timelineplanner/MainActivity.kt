1eacd89b27072c9c0fbf354f21210436c671cae3
Yebin Ahn <ayb0711@sookmyung.ac.kr>
2023-11-23 21:46:21 +0900

    시작화면과 회원가입 화면 intent 연결

--- app/src/main/java/com/example/timelineplanner/MainActivity.kt
+++ app/src/main/java/com/example/timelineplanner/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.timelineplanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.example.timelineplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnJoin1.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+   }
 }
+
+
