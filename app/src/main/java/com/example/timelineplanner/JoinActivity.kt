432f559e924a9b9aa1cb6972071042b19893c7ac
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2023-11-24 20:32:04 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/timelineplanner/JoinActivity.kt
@@ -0,0 +1,25 @@
+package com.example.timelineplanner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timelineplanner.databinding.ActivityJoinBinding
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnJoin2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
