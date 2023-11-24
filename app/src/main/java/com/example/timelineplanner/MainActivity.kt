1303836a80b8d5be7aca924f97a0ad3380c44495
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2023-11-24 20:00:41 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/timelineplanner/MainActivity.kt
+++ app/src/main/java/com/example/timelineplanner/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.timelineplanner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.timelineplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnJoin1.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
