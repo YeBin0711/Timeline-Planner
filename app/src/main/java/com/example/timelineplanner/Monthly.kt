bc9b3253a2001a122d4ae68ac54f453112932327
jo-hseo <ellejo1110@gmail.com>
2023-11-26 22:24:50 +0900

    월별 일정 레이아웃 완성

--- app/src/main/java/com/example/timelineplanner/Monthly.kt
+++ app/src/main/java/com/example/timelineplanner/Monthly.kt
@@ -1,10 +1,12 @@
 package com.example.timelineplanner
 
+import android.graphics.Color
 import android.view.View
+import android.view.ViewGroup
 import com.example.timelineplanner.databinding.CalendarCellBinding
-import com.example.timelineplanner.databinding.MonthlyHeaderBinding
 import com.kizitonwose.calendar.core.CalendarDay
 import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.ViewContainer
@@ -18,17 +20,21 @@ class MonthlyCellBinder : MonthDayBinder<CalendarCellContainer> {
     override fun bind(container: CalendarCellContainer, data: CalendarDay) {
         container.day = data
         container.binding.day.text = data.date.dayOfMonth.toString()
+        if (data.position == DayPosition.MonthDate) {
+            container.binding.day.setTextColor(Color.BLACK)
+        } else {
+            container.binding.day.setTextColor(Color.GRAY)
+        }
     }
 }
 
 
 class MonthlyHeaderContainer(view: View) : ViewContainer(view) {
-    val monthlyHeader = MonthlyHeaderBinding.bind(view)
+    val monthlyHeader = view as ViewGroup
 }
 
 class MonthlyHeaderBinder : MonthHeaderFooterBinder<MonthlyHeaderContainer> {
     override fun create(view: View) = MonthlyHeaderContainer(view)
     override fun bind(container: MonthlyHeaderContainer, month: CalendarMonth) {
-        container.monthlyHeader.month.text = month.yearMonth.toString()
     }
 }
--- app/src/main/java/com/example/timelineplanner/MonthlyActivity.kt
+++ app/src/main/java/com/example/timelineplanner/MonthlyActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.timelineplanner.databinding.ActivityMonthlyBinding
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.view.MarginValues
 import java.time.YearMonth
 
 class MonthlyActivity : AppCompatActivity() {
-    val binding = ActivityMonthlyBinding.inflate(layoutInflater)
+    lateinit var binding: ActivityMonthlyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMonthlyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calendarView.dayBinder = MonthlyCellBinder()
-        binding.calendarView.monthHeaderBinder = MonthlyHeaderBinder()
+        
 
         val currentMonth = YearMonth.now()
         val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
         val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
         val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
         binding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        binding.calendarView.monthMargins = MarginValues(15, 0, 15, 0)
         binding.calendarView.scrollToMonth(currentMonth)
+        binding.calendarView.dayBinder = MonthlyCellBinder()
+        binding.calendarView.monthHeaderBinder = MonthlyHeaderBinder()
+        binding.calendarView.monthScrollListener = { month ->
+            binding.calendarView.notifyMonthChanged(month.yearMonth)
+            binding.month.text = month.yearMonth.toString()
+        }
     }
 }
