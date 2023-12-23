b5d8d78149a337ff2490cd6ed79ab87a1510863e
YeBin0711 <ayb0711@sookmyung.ac.kr>
2023-12-23 13:19:17 +0900

    로그인 기능 구현, 앱 유무 말고 크롬으로 넘어가게 해서 구현

--- app/src/main/java/com/example/timelineplanner/MainActivity.kt
+++ app/src/main/java/com/example/timelineplanner/MainActivity.kt
@@ -30,15 +30,13 @@ class MainActivity : AppCompatActivity() {
 
         // 카카오톡 로그인 버튼 클릭 시
         binding.kakaoLoginBtn.setOnClickListener {
-            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+            UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
                 if (error != null) {
-                    Log.e(TAG, "카카오톡 로그인 실패: $error")
-                    Toast.makeText(baseContext, "카카오톡이 다운로드 되어있지 않습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(baseContext, "카카오 계정으로 로그인 실패: $error", Toast.LENGTH_SHORT).show()
                 } else if (token != null) {
-                    Log.i(TAG, "카카오톡 로그인 성공: ${token.accessToken}")
+                    Log.i(TAG, "카카오 계정으로 로그인 성공: ${token.accessToken}")
 
-                    // 여기서 로그인 성공 후의 동작 수행
-                    // 예: 홈 화면으로 이동
+                    // 로그인 성공 후의 동작 수행
                     val intent = Intent(this, HomeActivity::class.java)
                     startActivity(intent)
                     finish()
